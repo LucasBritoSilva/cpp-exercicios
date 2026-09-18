4d0208c924e2dc560a5182184bc0aa9e66ef17af
Lucas Brito <lucas.brito.silva.2008@gmail.com>
2026-09-18 18:50:03 -0400

    Adicionando struct no sistema de cadastro

--- 02-intermediario/sistema-cadastro.cpp
+++ 02-intermediario/sistema-cadastro.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-string nomes[100];
-string cpfs[100];
-float rendas[100];
-int idades[100];
-string ir[100];
-string aposentadoria[100];
+struct Pessoa {
+    string nome;
+    string cpf;
+    float renda;
+    int idade;
+    string ir;
+    string aposentadoria;
+};
+
 int totalpessoas = 0;
 
 string funcaoir(float renda) {
@@ -22,7 +26,9 @@ string funcaoir(float renda) {
 string funcaoapo(int idade) {
     if (idade <= 65) {
         idade = 65 - idade;
+
         string idadeconvert = to_string(idade);
+
         return "Faltam " + idadeconvert + " anos para aposentar";
     }
     else {
@@ -30,56 +36,67 @@ string funcaoapo(int idade) {
     }
 }
 
-void cadastro(string nome[], string cpf[], float renda[], int idade[], int pos)
-{
+void cadastro(Pessoa pessoas[], int pos) {
+
     cout << "Nome: ";
-    cin >> nome[pos];
+    cin >> pessoas[pos].nome;
 
     cout << "CPF: ";
-    cin >> cpf[pos];
+    cin >> pessoas[pos].cpf;
 
     cout << "Renda mensal: ";
-    cin >> renda[pos];
+    cin >> pessoas[pos].renda;
 
     cout << "Idade: ";
-    cin >> idade[pos];
+    cin >> pessoas[pos].idade;
 
-    ir[pos] = funcaoir(renda[pos]);
+    pessoas[pos].ir = funcaoir(pessoas[pos].renda);
 
-    aposentadoria[pos] = funcaoapo(idade[pos]);
+    pessoas[pos].aposentadoria = funcaoapo(pessoas[pos].idade);
 }
 
-
 int main() {
+
+    Pessoa pessoas[100];
+
     bool sair = false;
     int opcao;
 
     while (sair == false) {
+
         cout << "Digite 1 para Incluir" << endl;
         cout << "Digite 2 para Listar" << endl;
         cout << "Digite 3 para Sair" << endl;
+
         cin >> opcao;
 
         if (opcao == 1) {
-            cadastro(nomes, cpfs, rendas, idades, totalpessoas);
+
+            cadastro(pessoas, totalpessoas);
+
             totalpessoas++;
         }
+
         else if (opcao == 2) {
-            int i = 0;
-            for (i; i < totalpessoas; i++) {
-                    cout << endl << "Nome: " << nomes[i] << endl << endl;
-                    cout << "Cpf: " << cpfs[i] << endl;
-                    cout << "Renda: " << rendas[i] << endl;
-                    cout << "Idade: " << idades[i] << endl;
-                    cout << "IR: " << ir[i] << endl;
-                    cout << "Aposentadoria: " << aposentadoria[i] << endl << endl;
+
+            for (int i = 0; i < totalpessoas; i++) {
+
+                cout << endl;
+                cout << "Nome: " << pessoas[i].nome << endl;
+                cout << "Cpf: " << pessoas[i].cpf << endl;
+                cout << "Renda: " << pessoas[i].renda << endl;
+                cout << "Idade: " << pessoas[i].idade << endl;
+                cout << "IR: " << pessoas[i].ir << endl;
+                cout << "Aposentadoria: "
+                    << pessoas[i].aposentadoria << endl;
+                cout << endl;
             }
         }
+
         else if (opcao == 3) {
             sair = true;
         }
     }
-    
 
     return 0;
 }
