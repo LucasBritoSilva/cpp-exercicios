a4878a5d0b783c2860bb3b29859c55f4d1b2086e
Lucas Brito <lucas.brito.silva.2008@gmail.com>
2026-09-21 11:16:33 -0400

    Adicionando sistema de verificação de x no vetor

---
+++ 02-intermediario/x-esta-ou-n.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool decide(int a[], int n, int x) {
+    if (n == 0) {
+        return false;
+    }
+    if (a[n - 1] == x) {
+        return true;
+    }
+    return decide(a, n - 1, x);
+}
+
+int main() {
+    int n, x;
+    cin >> n;
+    cin >> x;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    if (decide(a, n, x)) {
+        cout << "x esta no vetor" << endl;
+    }
+    else {
+        cout << "x nao esta no vetor" << endl;
+    }
+    return 0;
+}
